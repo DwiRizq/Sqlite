517c97fcc887893c467777f3619bedfa93e4c087
Dwi Rizqi Oktaviani <dwi.rizqi.ft18@mail.umy.ac.id>
2020-04-21 00:47:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sqlite/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
